c983d2e4f0c4379b4158d9938cc34a86e0104da2
Dafna Hirschfeld <dafna3@gmail.com>
2018-12-17 03:10:35 -0800

    adding script that test the streamin with the v4l2-ctl util currently supports croping

---
+++ test-v4l2-utils-with-jelly.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -x
+
+if [ ! if ! md5sum -c jelly.md5 ]
+then
+	wget http://jell.yfish.us/media/jellyfish-10-mbps-hd-h264.mkv
+fi
+
+if [ ! -f images/jelly-1920-1080.YU12 ]
+then
+	ffmpeg -i jellyfish-10-mbps-hd-h264.mkv -c:v rawvideo -pix_fmt yuv420p -f rawvideo images/jelly-1920-1080.YU12
+fi
+if [ ! -f images/jelly-1920-1080.RGB3 ]
+then
+	ffmpeg -s 1920x1080 -pix_fmt yuv420p -f rawvideo -i images/jelly-1920-1080.YU12 -pix_fmt rgb24 -f rawvideo images/jelly-1920-1080.RGB3
+fi
+
+if [ ! -f images/jelly-902-902.RGB3 ]
+then
+	ffmpeg -s 1920x1080 -pix_fmt rgb24 -f rawvideo -i images/jelly-1920-1080.RGB3 -filter:v "crop=902:902:0:0" -pix_fmt rgb24  -f rawvideo images/jelly-902-902.RGB3
+fi
+
+
+
+function codec {
+
+   case "$5" in
+    YU12) ffp=yuv420p ;;
+    *) ffp=rgb24 ;;
+   esac
+
+   echo "ffp=$ffp"
+
+   v4l2-ctl -d0 --set-selection-output target=crop,width=$1,height=$2   -x width=$3,height=$4,pixelformat=$5 --stream-mmap --stream-out-mmap --stream-to jelly_$1-$2-$5.fwht --stream-from images/jelly-$3-$4.$5 --all
+
+   v4l2-ctl -d1 -x width=$1,height=$2 -v width=$1,height=$2,pixelformat=$5 --stream-mmap --stream-out-mmap --stream-from jelly_$1-$2-$5.fwht --stream-to out-$1-$2.$5 --all
+
+   ffplay -v info -f rawvideo -pixel_format $ffp -video_size $1x$2  out-$1-$2.$5
+
+}
+
+codec 902 902 902 902 RGB3
+
+codec 642 642 1920 1080 RGB3
+codec 642 642 1920 1080 YU12
+
+codec 1440 900  1920 1080 RGB3
+codec 1440 900  1920 1080 YU12
+
+codec 1920 1080 1920 1080 RGB3
+codec 1920 1080 1920 1080 YU12
+
+
